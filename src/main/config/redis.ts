29639dd4cdf559dff7744e9026b8526930a5d382
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-09-03 15:33:37 -0400

    feat: create instance and container to conect redis

--- src/main/config/redis.ts
+++ src/main/config/redis.ts
@@ -1,8 +1,12 @@
-import { Redis } from 'ioredis'
+import IORedis from 'ioredis'
 
-export class RedisService extends Redis {
+export class RedisService extends IORedis {
   constructor() {
-    super()
+    super({
+      host: process.env.REDIS_HOSTNAME,
+      port: 6379,
+      password: process.env.REDIS_PASSWORD
+    })
 
     super.on('error', err => {
       console.log('Error on Redis')
