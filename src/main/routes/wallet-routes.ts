df0a324100f508187d9b1c9d0ee1729d56a0238f
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-11-16 00:28:00 -0400

    fix: adjust import

--- src/main/routes/wallet-routes.ts
+++ src/main/routes/wallet-routes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express'
 import { adaptExpressRoute as adapt } from '../adapters'
 import {
   makeAddWalletController,
-  makeListWalletByIdController
+  makeListWalletByIdController,
+  makeListWalletsByProfileIdController
   // makeListMovieAllController,
   // makeListMovieByIdController
 } from '../factories/application/controllers'
-import { makeListWalletsByProfileIdController } from '../factories/application/controllers/list-wallets-by-profile-id-controller-factory'
 
 export default (router: Router): void => {
   router.post('/wallets', adapt(makeAddWalletController()))
