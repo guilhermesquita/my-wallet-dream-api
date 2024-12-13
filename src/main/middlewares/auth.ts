79fca1591d916f75fbd715011be17fe443961ddd
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-12-13 16:55:45 -0400

    feat: implement authentication middleware

--- src/main/middlewares/auth.ts
+++ src/main/middlewares/auth.ts
@@ -1,24 +1,21 @@
-// // import { InvalidTokenError } from '@/application/errors'
-// import { unauthorized } from '@/application/helpers'
-// import { JwtTokenHandler } from '@/infra/gateways'
-// import { NextFunction, Request, Response } from 'express'
+import { InvalidTokenError } from '@/application/errors'
+import { forbidden, unauthorized } from '@/application/helpers'
+import { JwtTokenHandler } from '@/infra/gateways'
+import { NextFunction, Request, Response } from 'express'
 
-// // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-// export const auth = async (req: Request, res: Response, next: NextFunction) => {
-//   const token = req.headers.authorization
-//   const auth = token?.split(' ')[1] as string
-//   if (!token) {
-//     return res.status(401).json(unauthorized())
-//   }
-//   res.
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization
+  const auth = token?.split(' ')[1] as string
+  if (!token) {
+    return res.status(401).json(unauthorized())
+  }
 
-//   // res.status(401).json(forbidden(new AccessDeniedError()))
-
-//   try {
-//     const jwtTokenHandler = new JwtTokenHandler()
-//     await jwtTokenHandler.validate({ token: auth })
-//     next()
-//   } catch (error) {
-//     // res.status(403).json(forbidden(new InvalidTokenError()))
-//   }
-// }
+  try {
+    const jwtTokenHandler = new JwtTokenHandler()
+    await jwtTokenHandler.validate({ token: auth })
+    next()
+  } catch (error) {
+    res.status(403).json(forbidden(new InvalidTokenError()))
+  }
+}
--- src/main/routes/dream-routes.ts
+++ src/main/routes/dream-routes.ts
@@ -4,11 +4,13 @@ import {
   makeAddDreamController,
   makeListDreamsByProfileIdController
 } from '../factories/application/controllers'
+import { auth } from '../middlewares'
 
 export default (router: Router): void => {
   router.post('/dreams', adapt(makeAddDreamController()))
   router.get(
     '/dreams/profile/:id',
+    auth,
     adapt(makeListDreamsByProfileIdController())
   )
 }
